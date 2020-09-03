9b0bdca3ab3e16e99f0d6fe28c056e571883ff17
Jon Haase <haase1020@gmail.com>
2020-09-03 08:07:30 -0400

    start javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+//HTML elements
+
+const statusDiv = document.querySelector('.status');
+const resetDiv = document.querySelector('.reset');
+const cellDivs = document.querySelectorAll('.game-cell');
+
+console.log(statusDiv);
+console.log(resetDiv);
+console.log(cellDivs);
