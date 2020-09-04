730bf4a93ba4d54aeaa9eae668bd0d1b10041f09
Jon Haase <haase1020@gmail.com>
2020-09-04 15:05:50 -0400

    working on app with noel

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const statusDiv = document.querySelector('.status');
 const resetDiv = document.querySelector('.reset');
 const cellDivs = document.querySelectorAll('.game-cell');
 
-console.log(statusDiv);
-console.log(resetDiv);
-console.log(cellDivs);
+// game variables
+let gameIsLive = true;
+let xIsNext = true;
+
+// event handlers
+const handleReset = (e) => {
+  console.log(e);
+};
+
+const handleCellClick = (e) => {
+  console.log(e.target.classList);
+};
+
+// event listeners
+resetDiv.addEventListener('click', handleReset);
+
+for (const cellDiv of cellDivs) {
+  cellDiv.addEventListener('click', handleCellClick);
+}
