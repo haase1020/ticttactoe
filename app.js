a3faf8d0d63ce6c1a1ef0e7f08f8edee6d46276a
Jon Haase <haase1020@gmail.com>
2020-09-05 07:46:42 -0400

    halfway through

--- app.js
+++ app.js
@@ -14,7 +14,20 @@ const handleReset = (e) => {
 };
 
 const handleCellClick = (e) => {
-  console.log(e.target.classList);
+  const classList = e.target.classList;
+  const location = classList[1];
+
+  if (classList[2] === 'x' || classList[2] === 'o') {
+    return;
+  }
+
+  if (xIsNext) {
+    classList.add('x');
+    xIsNext = !xIsNext;
+  } else {
+    classList.add('o');
+    xIsNext = !xIsNext;
+  }
 };
 
 // event listeners
